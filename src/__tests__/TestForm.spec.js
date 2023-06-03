e290e4fac37efa633c822cb5c84a7b3e9731b452
51fe <riafan@hotmail.com>
2023-06-03 20:00:04 +0800

    vitest and vtu

--- src/__tests__/TestForm.spec.js
+++ src/__tests__/TestForm.spec.js
@@ -1,59 +1,63 @@
-import { fireEvent, screen, render, within } from '../test-util'
+import { vi } from 'vitest'
+import { mount, nextTick } from '../test-util'
 import TestForm from '../TestForm.vue'
 import { rules } from '../config'
 
 const setup = () => {
-  const result = render(TestForm)
-  const ageItem = screen.getByLabelText(/age/i)
-  const ageInput = within(ageItem).getByRole('textbox')
+  const wrapper = mount(TestForm)
+  const ageInput = wrapper.find('input')
+  const submitBtn = wrapper.find('.submit-btn')
   return {
-    ...result,
-    ageInput
+    wrapper,
+    ageInput,
+    submitBtn
   }
 }
 
-const requiredRE = new RegExp(rules[0].message)
-const numberRE = new RegExp(rules[1].message)
-
 beforeEach(() => {
-  jest.spyOn(console, 'warn').mockImplementation(() => { })
+  vi.spyOn(console, 'warn').mockImplementation(() => { })
 })
 
 afterEach(() => {
-  console.warn.mockRestore()
+  console.warn.mockClear()
+  vi.useRealTimers()
 })
 
 test('shows required error message without input', async () => {
-  const { ageInput } = setup()
-  await fireEvent.blur(ageInput)
-  expect(await screen.findByText(requiredRE)).toBeInTheDocument()
+  const { wrapper, submitBtn } = setup()
+  vi.useFakeTimers()
+  await submitBtn.trigger('click')
+  // bug: on vitest, formEl.validate() always returns true
+  vi.runOnlyPendingTimers()
+  await nextTick()
+  expect(wrapper.get('.el-form-item__error').text()).toContain(rules[0].message)
   expect(console.warn).toHaveBeenCalled()
 })
 
 test('shows number error message when input a string', async () => {
-  const { ageInput } = setup()
-  await fireEvent.update(ageInput, 'abc')
-  await fireEvent.blur(ageInput)
-  expect(await screen.findByText(numberRE)).toBeInTheDocument()
+  const { wrapper, ageInput, submitBtn } = setup()
+  vi.useFakeTimers()
+  await ageInput.setValue('abc')
+  await submitBtn.trigger('click')
+  // bug: on vitest, formEl.validate() always returns true
+  vi.runOnlyPendingTimers()
+  await nextTick()
+  expect(wrapper.get('.el-form-item__error').text()).toContain(rules[0].message)
   expect(console.warn).toHaveBeenCalled()
 })
 
 test('form is valid when input a number', async () => {
-  const { ageInput } = setup()
-  await fireEvent.update(ageInput, 20)
-  const submitBtn = screen.getByRole('button', { name: /submit/i })
-  await fireEvent.click(submitBtn)
-  expect(screen.queryByText(requiredRE)).not.toBeInTheDocument()
-  expect(screen.queryByText(numberRE)).not.toBeInTheDocument()
+  const { wrapper, ageInput } = setup()
+  await ageInput.setValue(20)
+  await ageInput.trigger('blur')
+  expect(wrapper.find('.el-form-item__error').exists()).toBeFalsy()
   expect(console.warn).not.toHaveBeenCalled()
 })
 
 test('resets form when click Reset button', async () => {
-  const { ageInput } = setup()
-  const submitBtn = screen.getByRole('button', { name: /reset/i })
-  await fireEvent.click(submitBtn)
-  expect(ageInput).toHaveValue('')
-  expect(screen.queryByText(requiredRE)).toBeNull()
-  expect(screen.queryByText(numberRE)).toBeNull()
+  const { wrapper, ageInput } = setup()
+  await wrapper.find('.reset-btn').trigger('click')
+  expect(ageInput.text()).toBe('')
+  expect(wrapper.find('.el-form-item__error').exists()).toBeFalsy()
   expect(console.warn).not.toHaveBeenCalled()
 })
