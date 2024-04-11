c6a004dc023e0c577c73ebcddb0b3092c69bb407
51fe <riafan@hotmail.com>
2024-04-11 17:19:03 +0800

    add ci

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
 import TestForm from './TestForm.vue'
-import TestDatePicker from './TestDatePicker.vue'
-import TestSelect from './TestSelect.vue'
 </script>
 <template>
   <TestForm />
-  <TestDatePicker />
-  <TestSelect />
 </template>
